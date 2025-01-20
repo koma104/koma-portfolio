fc667510bf7d1726d82bea0f079db2cbb7bc8afa
TOSHIHITO KOMATSU <miracle.komatsu@gmail.com>
2025-01-21 07:40:49 +0900

    fix: iphoneSE崩れ

--- react-todo/src/components/InputTodo.jsx
+++ react-todo/src/components/InputTodo.jsx
@@ -23,7 +23,7 @@ export const InputTodo = (props) => {
 const SContentWrapper = styled.div`
   display: grid;
   column-gap: 16px;
-  grid-template-columns: auto 80px;
+  grid-template-columns: auto 72px;
 `
 
 const SInput = styled.input`
