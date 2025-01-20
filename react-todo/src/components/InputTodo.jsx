e902cdaf1c16ce7ef5ca8331411263774d3790e7
TOSHIHITO KOMATSU <miracle.komatsu@gmail.com>
2025-01-21 07:26:21 +0900

    fix: 追加ボタン幅調整

--- react-todo/src/components/InputTodo.jsx
+++ react-todo/src/components/InputTodo.jsx
@@ -23,7 +23,7 @@ export const InputTodo = (props) => {
 const SContentWrapper = styled.div`
   display: grid;
   column-gap: 16px;
-  grid-template-columns: auto 90px;
+  grid-template-columns: auto 80px;
 `
 
 const SInput = styled.input`
