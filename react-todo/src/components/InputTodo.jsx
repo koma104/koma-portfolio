ce42ae1a48f082dfe7a5a763108e7f15b2492b57
TOSHIHITO KOMATSU <miracle.komatsu@gmail.com>
2025-01-21 01:11:48 +0900

    fix: 追加ボタン css修正

--- react-todo/src/components/InputTodo.jsx
+++ react-todo/src/components/InputTodo.jsx
@@ -11,9 +11,9 @@ export const InputTodo = (props) => {
         onChange={onChange}
         disabled={disabled}
       />
-      <button onClick={onClick} disabled={disabled}>
+      <SButton onClick={onClick} disabled={disabled}>
         追加
-      </button>
+      </SButton>
     </SInputTodo>
   );
 };
@@ -42,3 +42,16 @@ const SInputTodo = styled.div`
   box-shadow: 7px 7px 15px rgba(55, 84, 170, 0.15),
     -7px -7px 20px rgba(255, 255, 255, 1), 0px 0px 4px rgba(255, 255, 255, 0.2);
 `;
+
+const SButton = styled.button`
+  padding-block: 8px;
+  padding-inline: 20px;
+  border: none;
+  background-color: #ebecf0;
+  border-radius: 100vmax;
+  box-shadow: 7px 7px 15px rgba(55, 84, 170, 0.15), -7px -7px 20px rgba(255, 255, 255, 1),
+    inset 0px 0px 4px rgba(255, 255, 255, 0.2), inset 7px 7px 15px rgba(55, 84, 170, 0),
+    inset -7px -7px 20px rgba(255, 255, 255, 0), 0px 0px 4px rgba(255, 255, 255, 0);
+  transition: box-shadow 0.25s ease;
+  color: #000;
+`
