19dca46e81580381f5589640c86f8f73be47bd40
TOSHIHITO KOMATSU <miracle.komatsu@gmail.com>
2025-01-21 23:45:11 +0900

    todo: input崩れ

--- react-todo/src/components/InputTodo.jsx
+++ react-todo/src/components/InputTodo.jsx
@@ -23,7 +23,7 @@ export const InputTodo = (props) => {
 const SContentWrapper = styled.div`
   display: grid;
   column-gap: 16px;
-  grid-template-columns: 1fr minmax(90px, auto);
+  grid-template-columns: auto minmax(90px, auto);
 `
 
 const SInput = styled.input`
